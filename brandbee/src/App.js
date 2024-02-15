ed7db06d7638fa16e7d0840e99431eecdef68fda
Rohitsharma7877 <rohitsharma.sharma194@gmail.com>
2024-02-15 13:35:18 +0530

    edit

--- brandbee/src/App.js
+++ brandbee/src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Navbar />
       {/* <DroupDown/> */}
-      <DroupDownImage/>
+      {/* <DroupDownImage/> */}
 
       <MainRouter />
       <Footer />     
